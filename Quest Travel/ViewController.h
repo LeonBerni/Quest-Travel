14975c840d347d675ec76728c3a090f3012da643
iOS Treinamento <leonberni@hotmail.com>
2014-03-26 17:47:53 -0300

    pin e location

--- Quest Travel/ViewController.h
+++ Quest Travel/ViewController.h
@@ -7,7 +7,9 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <MapKit/MapKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <MKMapViewDelegate>
+@property (strong, nonatomic) IBOutlet MKMapView *mapView;
 
 @end
--- Quest Travel/ViewController.m
+++ Quest Travel/ViewController.m
@@ -18,6 +18,10 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    self.mapView.delegate = self;
+    [self iniciaComPontos];
+    [[self mapView] setShowsUserLocation:YES];
+    [self addGestureRecogniserToMapView];
 }
 
 - (void)didReceiveMemoryWarning
@@ -25,5 +29,55 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (IBAction)mapControl:(UISegmentedControl *)sender {
+        switch (sender.selectedSegmentIndex) {
+            case 0:
+                self.mapView.mapType = MKMapTypeStandard;
+                break;
+            case 1:
+                self.mapView.mapType = MKMapTypeSatellite;
+                break;
+            case 2:
+                self.mapView.mapType = MKMapTypeHybrid;
+                break;
+            default:
+                break;
+        }
+}
+
+- (void)addGestureRecogniserToMapView{
+    
+    UILongPressGestureRecognizer *lpgr = [[UILongPressGestureRecognizer alloc]
+                                          initWithTarget:self action:@selector(addPinToMap:)];
+    lpgr.minimumPressDuration = 0.5; //
+    [self.mapView addGestureRecognizer:lpgr];
+    
+}
+
+
+
+
+- (void) iniciaComPontos {
+    CLGeocoder *geocoder = [[CLGeocoder alloc] init];
+    [geocoder geocodeAddressString: @"Alameda Santos" completionHandler:^(NSArray *placemarks, NSError *error) {
+        for (CLPlacemark* aPlacemark in placemarks) {
+            CLLocationCoordinate2D coordinate;
+            MKPointAnnotation *anotacao = [[MKPointAnnotation alloc] init];
+            coordinate.latitude = [[NSString stringWithFormat:@"%f",aPlacemark.location.coordinate.latitude] doubleValue];
+            coordinate.longitude = [[NSString stringWithFormat:@"%f",aPlacemark.location.coordinate.longitude] doubleValue];
+            anotacao.coordinate = coordinate;
+            anotacao.title = @"teste";
+            
+            [[self mapView] addAnnotation: anotacao];
+        }
+    }];
+}
+
+
+- (void)viewWillAppear:(BOOL)animated {
+    [self.mapView setUserTrackingMode:MKUserTrackingModeFollow animated:YES];
+}
+
 
 @end
