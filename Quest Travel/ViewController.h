44b2b045b0c352d69d241cc570bbe3a43c065259
LEONARDO LEGRADY SALGADO GIGLIOTTI <leonardo.gigliotti@FreeHearthStoneDownload.local>
2014-03-27 18:22:39 -0300

    leo_commit

--- Quest Travel/ViewController.h
+++ Quest Travel/ViewController.h
@@ -9,7 +9,9 @@
 #import <UIKit/UIKit.h>
 #import <MapKit/MapKit.h>
 
-@interface ViewController : UIViewController <MKMapViewDelegate>
+@interface ViewController : UIViewController <MKMapViewDelegate> {
+    MKUserLocation *locationUsuario;
+}
 @property (strong, nonatomic) IBOutlet MKMapView *mapView;
 
 @end
--- Quest Travel/ViewController.m
+++ Quest Travel/ViewController.m
@@ -19,9 +19,8 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
     self.mapView.delegate = self;
-    [self iniciaComPontos];
+    [self procuraEndereço];
     [[self mapView] setShowsUserLocation:YES];
-    [self addGestureRecogniserToMapView];
 }
 
 - (void)didReceiveMemoryWarning
@@ -31,43 +30,40 @@
 }
 
 - (IBAction)mapControl:(UISegmentedControl *)sender {
-        switch (sender.selectedSegmentIndex) {
-            case 0:
-                self.mapView.mapType = MKMapTypeStandard;
-                break;
-            case 1:
-                self.mapView.mapType = MKMapTypeSatellite;
-                break;
-            case 2:
-                self.mapView.mapType = MKMapTypeHybrid;
-                break;
-            default:
-                break;
-        }
-}
-
-- (void)addGestureRecogniserToMapView{
-    
-    UILongPressGestureRecognizer *lpgr = [[UILongPressGestureRecognizer alloc]
-                                          initWithTarget:self action:@selector(addPinToMap:)];
-    lpgr.minimumPressDuration = 0.5; //
-    [self.mapView addGestureRecognizer:lpgr];
-    
+    switch (sender.selectedSegmentIndex) {
+        case 0:
+            self.mapView.mapType = MKMapTypeStandard;
+            break;
+        case 1:
+            self.mapView.mapType = MKMapTypeSatellite;
+            break;
+        case 2:
+            self.mapView.mapType = MKMapTypeHybrid;
+            break;
+        default:
+            break;
+    }
 }
 
 
-
-
-- (void) iniciaComPontos {
+- (void) procuraEndereço {
     CLGeocoder *geocoder = [[CLGeocoder alloc] init];
-    [geocoder geocodeAddressString: @"Alameda Santos" completionHandler:^(NSArray *placemarks, NSError *error) {
+    CLLocationCoordinate2D centro;
+    centro.latitude = locationUsuario.location.coordinate.latitude;
+    centro.longitude = locationUsuario.location.coordinate.longitude;
+    CLLocationDistance distancia = [self distanceFromUser];
+    CLCircularRegion *regiaoDeBusca = [[CLCircularRegion alloc] initWithCenter: centro radius: distancia identifier:@"regiaoDeBusca"];
+    [geocoder geocodeAddressString: @"Alameda Santos 1325" inRegion:regiaoDeBusca completionHandler:^(NSArray *placemarks, NSError *error)  {
         for (CLPlacemark* aPlacemark in placemarks) {
             CLLocationCoordinate2D coordinate;
             MKPointAnnotation *anotacao = [[MKPointAnnotation alloc] init];
             coordinate.latitude = [[NSString stringWithFormat:@"%f",aPlacemark.location.coordinate.latitude] doubleValue];
             coordinate.longitude = [[NSString stringWithFormat:@"%f",aPlacemark.location.coordinate.longitude] doubleValue];
             anotacao.coordinate = coordinate;
-            anotacao.title = @"teste";
+            
+            
+            
+            anotacao.title = aPlacemark.country;
             
             [[self mapView] addAnnotation: anotacao];
         }
@@ -75,9 +71,29 @@
 }
 
 
+- (double)distanceFromUser
+{
+    double zoomer = MKMetersBetweenMapPoints(MKMapPointForCoordinate(self.mapView.userLocation.location.coordinate), self.mapView.visibleMapRect.origin);
+    
+    if (zoomer > 50000) {
+        zoomer = 50000;
+    }
+    
+    return zoomer;
+}
+
+
+- (void) mapView:(MKMapView *)mapView didUpdateUserLocation:(MKUserLocation *)userLocation {
+    locationUsuario = userLocation;
+}
+
 - (void)viewWillAppear:(BOOL)animated {
     [self.mapView setUserTrackingMode:MKUserTrackingModeFollow animated:YES];
 }
 
-
+- (void)mapViewDidFinishRenderingMap:(MKMapView *)mapView fullyRendered:(BOOL)fullyRendered {
+    if (fullyRendered) {
+        NSLog(@"%f", [self distanceFromUser]);
+    }
+}
 @end
