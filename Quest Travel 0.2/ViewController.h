5e5afb1945f0f4acf915f1421f7e3bf8d7a6830c
Leonardo Gigliotti <leogig.lol@live.com>
2014-04-03 00:18:07 -0300

    Initial Commit

---
+++ Quest Travel 0.2/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Quest Travel 0.2
+//
+//  Created by Leo Gigliotti on 4/3/14.
+//  Copyright (c) 2014 Leo Gigliotti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Quest Travel 0.2/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Quest Travel 0.2
+//
+//  Created by Leo Gigliotti on 4/3/14.
+//  Copyright (c) 2014 Leo Gigliotti. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
