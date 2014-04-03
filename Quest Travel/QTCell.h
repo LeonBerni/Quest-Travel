f80fafa70d6fafd5ebbfa62636fc92b73ed47712
Rodrigo P. Assunção <rodrigo.pereiraassuncao@gmail.com>
2014-04-03 15:05:16 -0300

    Adicionada Custom Cell

---
+++ Quest Travel/QTCell.h
@@ -0,0 +1,13 @@
+//
+//  QTCell.h
+//  Quest Travel
+//
+//  Created by Rodrigo P. Assunção on 03/04/14.
+//  Copyright (c) 2014 LEON SALGUEIRO VALIENGO BERNI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QTCell : UITableViewCell
+
+@end
