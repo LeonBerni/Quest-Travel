897c0b59d2fe0672070a3091d23c3a7e464e62e3
Rodrigo P. Assunção <rodrigo@DelCtrl-Mac.local>
2014-04-03 00:22:38 -0300

    Adicionadas Classes para lidar com as quests e pontos de interesse

---
+++ Quest Travel/QTQuestTableController.m
@@ -0,0 +1,13 @@
+//
+//  QTQuestTableController.m
+//  Quest Travel
+//
+//  Created by Rodrigo P. Assunção on 02/04/14.
+//  Copyright (c) 2014 LEON SALGUEIRO VALIENGO BERNI. All rights reserved.
+//
+
+#import "QTQuestTableController.h"
+
+@implementation QTQuestTableController
+
+@end
